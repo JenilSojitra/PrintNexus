f8df4513eac94790a1207f09ae1ad655ced9ad88
Jenil Sojitra <119627051+JenilSojitra@users.noreply.github.com>
2024-12-30 10:18:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="PrintNexus/" element={<Home />} />
         <Route path="/collection" element={<Collections />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
